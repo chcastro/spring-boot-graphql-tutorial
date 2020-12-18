87ea990f3abc3554377b6113f0ded000568c2cf5
F. Christian Castro <f.christian.castro@gmail.com>
2020-12-17 23:48:36 -0600

    Configure server client

--- src/main/java/com/corelogic/example/springbootgraphqltutorial/resolver/SubscriptionResolver.java
+++ src/main/java/com/corelogic/example/springbootgraphqltutorial/resolver/SubscriptionResolver.java
@@ -5,6 +5,7 @@ import com.corelogic.example.springbootgraphqltutorial.repository.entity.Custome
 import com.corelogic.example.springbootgraphqltutorial.repository.repository.CustomerRepository;
 import graphql.kickstart.tools.GraphQLSubscriptionResolver;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -13,12 +14,18 @@ import java.time.Duration;
 import java.util.List;
 
 @Component
+@Slf4j
 @AllArgsConstructor
 public class SubscriptionResolver implements GraphQLSubscriptionResolver {
 
   private final CustomerRepository customerRepository;
 
   public Publisher<Customer> allCustomers() {
+    log.trace("This is a TRACE level message");
+    log.debug("This is a DEBUG level message");
+    log.info("This is an INFO level message");
+    log.warn("This is a WARN level message");
+    log.error("This is an ERROR level message");
     List<CustomerEntity> customers = customerRepository.findAll();
     Flux<CustomerEntity> customerEntityFlux = Flux.fromStream(customers.stream());
     Flux<Long> durationFlux = Flux.interval(Duration.ofSeconds(5));
